15c05afb259eecc43746a07851252bd9ab895507
SarojaRk <129292821+SarojaRk@users.noreply.github.com>
2023-04-15 00:48:40 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "tf-sg" {
+  name        = "security via terraform"
+  description = "security via terraform"
+  vpc_id      = "vpc-0f460e54c3d4fe2d6"
+
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ ingress {
+    description      = "https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "tf-sg"
+  }
+}
