c47021534c9d00346d4c1df82b4200ab06327c58
SarojaRk <129292821+SarojaRk@users.noreply.github.com>
2023-04-15 02:11:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
 ami = "ami-07d3a50bd29811cd1"
 instance_type = "t2.micro"
  vpc_security_group_ids = [aws_security_group.tf-sg.id]
+ user_data = file("world.sh")
 
 
 #one way to generate key create from console and add here ssh-22 port shuld be open
