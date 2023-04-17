2554efc435fd63c61a86a07898098efe61f96d22
SarojaRk <129292821+SarojaRk@users.noreply.github.com>
2023-04-18 01:04:24 +0530

    Create Db.tf

---
+++ Db.tf
@@ -0,0 +1,18 @@
+# deploy MySQL Database on AWS RDS 
+
+resource "aws_db_instance" "myrds" {
+    allocated_storage   = var.dbstorage
+   storage_type        = "gp2"
+   identifier          = "rdstf"
+   engine              = "mysql"
+   engine_version      = "8.0.27"
+   instance_class      = "db.t2.micro"
+   username            = "admin"
+   password            = "Passw0rd!123"
+   publicly_accessible = true
+   skip_final_snapshot = true
+
+   tags = {
+     Name = "MyRDS"
+   }
+ }
